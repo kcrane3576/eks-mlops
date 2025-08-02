600be48540697192dd7cacdb3ad664f65c6ec2b2
kcrane <kcrane121@gmail.com>
2025-08-02 17:30:23 -0400

    init eks

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,23 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4" # latest as of Aug 2025
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = var.private_subnet_ids
+  vpc_id     = var.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+
+  tags = var.tags
+}
