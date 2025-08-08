ecd3c5d72813c7d8169532e784018dadfaafddc4
kcrane <kcrane121@gmail.com>
2025-08-08 11:54:22 -0400

    added config to create sg inside default managed node group

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -17,6 +17,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
+      create_security_group = true
       force_update_version = true
       instance_types = ["t3.medium"]
       min_size       = 1
