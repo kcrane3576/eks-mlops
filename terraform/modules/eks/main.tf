6500f4dc343ff123adfc816f58cce4cad670edf0
kcrane <kcrane121@gmail.com>
2025-08-08 11:54:46 -0400

    format

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -18,11 +18,11 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       create_security_group = true
-      force_update_version = true
-      instance_types = ["t3.medium"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      force_update_version  = true
+      instance_types        = ["t3.medium"]
+      min_size              = 1
+      max_size              = 3
+      desired_size          = 2
 
       # Optionally add labels, taints, or extra configs
       labels = {
