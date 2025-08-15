7ca1715f5f926cd90d94fb25f8ff97a4183f4a49
kcrane <kcrane121@gmail.com>
2025-08-15 16:56:50 -0400

    consolidated providers

--- terraform/envs/dev/k8s_providers.tf
+++ terraform/envs/dev/k8s_providers.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.30" }
-    helm       = { source = "hashicorp/helm", version = "~> 2.13" }
-  }
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
