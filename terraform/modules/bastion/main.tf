91a2f2e283b51049404b86be8e6e68845bf17e3e
kcrane <kcrane121@gmail.com>
2025-08-16 13:32:32 -0400

    removed bastion eks admin access

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -221,17 +221,3 @@ resource "aws_instance" "bastion" {
     S3GatewayEndpointId = var.s3_gateway_endpoint_id
   })
 }
-
-resource "aws_eks_access_entry" "bastion" {
-  cluster_name  = var.cluster_name
-  principal_arn = aws_iam_role.bastion.arn
-  type          = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "bastion_admin" {
-  cluster_name  = var.cluster_name
-  principal_arn = aws_iam_role.bastion.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  access_scope { type = "cluster" }
-  depends_on = [aws_eks_access_entry.bastion]
-}
