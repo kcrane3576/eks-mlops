b27f9f220d40ff889ea0a76c762b33fdfdcf3970
kcrane <kcrane121@gmail.com>
2025-08-23 09:13:41 -0400

    remove "policy" from reference for name and arn

--- terraform/modules/iam/github_oidc_roles/read_policies/vpc_state/outputs.tf
+++ terraform/modules/iam/github_oidc_roles/read_policies/vpc_state/outputs.tf
@@ -1,7 +1,7 @@
 output "policy_arn" {
-  value = aws_iam_policy.gh_ci_vpc_state_read.policy.arn
+  value = aws_iam_policy.gh_ci_vpc_state_read.arn
 }
 
 output "policy_name" {
-  value = aws_iam_policy.gh_ci_vpc_state_read.policy.name
+  value = aws_iam_policy.gh_ci_vpc_state_read.name
 }
