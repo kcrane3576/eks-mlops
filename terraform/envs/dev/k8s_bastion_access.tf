1eb34e1373839071de09a4c895a2866464c8b184
kcrane <kcrane121@gmail.com>
2025-08-24 13:07:25 -0400

    add missing provider config

--- terraform/envs/dev/k8s_bastion_access.tf
+++ terraform/envs/dev/k8s_bastion_access.tf
@@ -13,7 +13,7 @@ resource "aws_iam_role" "ops_admin" {
   name               = "${var.environment}-ops-admin"
   assume_role_policy = data.aws_iam_policy_document.ops_admin_trust.json
   tags = merge(
-    local.default_tags, {
+    local.policy_tags, {
       Name = "${var.environment}-ops-admin"
     }
   )
@@ -39,17 +39,20 @@ resource "aws_iam_policy" "ops_admin_read" {
 }
 
 resource "aws_iam_role_policy_attachment" "ops_admin_read_attach" {
+  provider   = aws.ci_write_role
   role       = aws_iam_role.ops_admin.name
   policy_arn = aws_iam_policy.ops_admin_read.arn
 }
 
 resource "aws_eks_access_entry" "ops_admin" {
+  provider      = aws.ci_write_role
   cluster_name  = module.eks.cluster_name
   principal_arn = aws_iam_role.ops_admin.arn
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "ops_admin_admin" {
+  provider      = aws.ci_write_role
   cluster_name  = module.eks.cluster_name
   principal_arn = aws_iam_role.ops_admin.arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
