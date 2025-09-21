dfb1a72a74837c8767020194f6e00cb0370f45c0
kcrane <kcrane121@gmail.com>
2025-09-21 15:47:38 -0400

    removed config to assume write role

--- terraform/envs/dev/providers.tf
+++ terraform/envs/dev/providers.tf
@@ -5,13 +5,3 @@ locals {
 provider "aws" {
   region = var.region
 }
-
-provider "aws" {
-  alias  = "ci_write_role"
-  region = var.region
-  assume_role {
-    role_arn     = var.ci_write_role_arn
-    session_name = "tf_ci_write_role"
-    external_id  = local.provider_external_id
-  }
-}
